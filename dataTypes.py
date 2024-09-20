dc2b0ea9c743512a28b992a26a02fd04f5fd502a
Frank Kocun <frankkocun@gmail.com>
2024-09-19 22:25:49 -0400

    started to work on main function. datatypes are done. consider if it is better to use regular dict instead of custom

--- dataTypes.py
+++ dataTypes.py
@@ -17,7 +17,18 @@ class SmartTuple():
         >>> s2 = SmartTuple((10, 10, 20), 10)
         >>> s1 == s2
         poop
-
+        >>> s1 = SmartTuple((20, 20, 20), 10)
+        >>> s2 = SmartTuple((20, 20, 20), 10)
+        >>> s1 == s2
+        1.0
+        >>> s1 = SmartTuple((20, 20, 20), 6)
+        >>> s2 = SmartTuple((10, 10, 15), 6)
+        >>> s1 == s2
+        0.3
+        >>> s1 = SmartTuple((5,), 20)
+        >>> s2 = SmartTuple((355,), 20)
+        >>> s1 == s2
+        1
 
         """
         score = 0
@@ -25,14 +36,38 @@ class SmartTuple():
         compared_len = len(min(self.items, other.items))
         for i in range(compared_len):
             distance = abs(self.items[i] - other.items[i])
-            score += (distance / self.tolerance) * i / 0.5
+            if distance >= 360 - self.tolerance:
+                distance = 360 - distance
+            if distance <= self.tolerance:
+                score += (self.tolerance - distance) / self.tolerance
+
         return score / compared_len
 
     def __len__(self):
         return len(self.items)
 
 
-
-
 class FrequencyDict:
-    pass
+
+    items: list[any]
+    freq: list[int]
+
+    def __init__(self, items: list[any], freq: list[int]):
+        self.items = items
+        self.freq = freq
+
+    def append(self, item: any, freq: int):
+        if item in self:
+            for i in range(len(self.items)):
+                if self.items[i] == item > 0:
+                    self.freq[i] += freq
+        else:
+            self.items.append(item)
+            self.freq.append(freq)
+
+    def __contains__(self, item: any):
+        for i in self.items:
+            if i == item:
+                return True
+        return False
+
--- main.py
+++ main.py
@@ -1,5 +1,44 @@
-# a subclass of a python tuple that checks equality of two tuples using a range of tolerance
-class ToleranceTuple(tuple):
-    def __eq__(self, other):
-        return all(abs(a - b) <= 20 for a, b in zip(self, other))
-    
+from dataTypes import FrequencyDict
+from dataTypes import SmartTuple
+
+
+def number_to_degrees(num):
+    """
+    Converts a number to degrees on a roulette wheel
+
+    >>> number_to_degrees(0)
+    4.85
+    >>> number_to_degrees(32)
+    14.549999999999999
+    >>> number_to_degrees(35)
+    334.65
+
+    """
+    nums = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8,
+            23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12,
+            35, 3, 26]
+    return (nums.index(num) + 1) * 9.7 - 4.85
+
+
+if __name__ == '__main__':
+
+    results = {}
+    window = []
+    user_input = ''
+
+    while user_input != 'q':
+        user_input = input('Enter a number or q to quit: ')
+        if user_input == 'q':
+            break
+        converted = number_to_degrees(int(user_input))
+
+        if len(window) > 5:
+            if window in results:
+                results[window].append(converted, 1)
+            else:
+                results[window] = FrequencyDict([converted], [1])
+            window.pop(0)
+
+        window.append(converted)
+
+
